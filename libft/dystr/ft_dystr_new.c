639d27d7932633460654db61e2b4b8cc5eb80f81
Jeremie MARTEL <jmartel@e2r13p8.42.fr>
2019-03-19 19:23:17 +0100

    fixing ft_dystr_new in libft (lexer branch)

--- libft/dystr/ft_dystr_new.c
+++ libft/dystr/ft_dystr_new.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 08:41:05 by jmartel           #+#    #+#             */
-/*   Updated: 2019/03/19 09:16:24 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/03/19 15:27:52 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,23 @@ t_dystr		*ft_dystr_new(char *str, size_t size, size_t len)
 {
 	t_dystr		*dystr;
 
+
 	if (!(dystr = malloc(sizeof(*dystr))))
 		return (NULL);
+	if (!str)
+	{
+		dystr->len = 0;
+		dystr->str = NULL;;
+		dystr->size = 0;
+		return (dystr);
+	}
 	if (!(dystr->str = malloc(sizeof(char) * (size + 1))))
 	{
 		free(dystr);
 		return (NULL);
 	}
-	if (str)
-		ft_strncpy(dystr->str, str, len);
-	if (!str)
-		dystr->len = 0;
-	else
-		dystr->len = len;
+	ft_strncpy(dystr->str, str, size);
+	dystr->len = len;
 	dystr->size = size;
 	return (dystr);
 }
