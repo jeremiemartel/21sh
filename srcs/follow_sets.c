49779dc488477f2589ababb53cfd4b1fbf17f54c
Ludovic DEDIER <ldedier@e3r10p12.42.fr>
2019-03-28 18:07:05 +0100

    fixed follow set computations

--- srcs/follow_sets.c
+++ srcs/follow_sets.c
@@ -6,7 +6,7 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/05 18:46:44 by ldedier           #+#    #+#             */
-/*   Updated: 2019/03/28 13:30:39 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/03/28 18:06:34 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,6 @@ int		sh_process_follow_sets_symbol_ref_prod(t_cfg *cfg, t_symbol *prod_symbol,
 	t_symbol	*symbol;
 	char		first_sets[NB_TERMS];
 	int			changes;
-
 	changes = 0;
 	ptr = production->symbols;
 	while (ptr != NULL)
@@ -77,11 +76,29 @@ int		sh_process_follow_sets_symbol_ref_prod(t_cfg *cfg, t_symbol *prod_symbol,
 				*/
 				sh_compute_first_sets_str(cfg, first_sets, ptr->next);
 				
-				/*sh_process_print_set(cfg, first_sets);
-				ft_printf("\nfollow_sets de ");
-				sh_print_symbol(ref);
-				ft_printf(":\n");
-				*/
+				
+				if (ref == &cfg->symbols[C])
+				{
+					sh_process_print_set(cfg, first_sets);
+					ft_printf("\nfollow_sets de ");
+					sh_print_symbol(ref);
+					ft_printf(":\n");
+					ft_printf("OLALALA\n");
+					ft_printf("ref: ");
+					sh_print_symbol(ref);
+					ft_printf("\nprod: ");
+					sh_print_symbol_list(ptr->next);
+					ft_printf("\nfrom ");
+					sh_print_symbol(prod_symbol);
+					ft_printf("\n");
+					ft_printf("beta first sets:\n");
+					sh_process_print_set(cfg, first_sets);
+					ft_printf("\n");
+					sh_print_symbol(ref);
+					ft_printf(" follow sets:\n");
+					sh_process_print_set(cfg, ref->follow_sets);
+					ft_printf("\n");
+				}
 				sh_process_fill_sets(first_sets, ref->follow_sets, 1, &changes);
 				/*
 				sh_process_print_set(cfg, ref->follow_sets);
@@ -89,15 +106,33 @@ int		sh_process_follow_sets_symbol_ref_prod(t_cfg *cfg, t_symbol *prod_symbol,
 				*/
 				if (first_sets[EPS]) //(2) b)
 				{
+					sh_print_symbol(prod_symbol);
+					ft_printf(GREEN" follow sets:\n"EOC);
+					sh_process_print_set(cfg, prod_symbol->follow_sets);
+					ft_printf("\n");
 					sh_process_fill_sets(prod_symbol->follow_sets,
 						ref->follow_sets, 0, &changes);
 				}
 			}
 			else // (2) a)
 			{
-					ft_printf("OLALALA\n");
+				if (ref == &cfg->symbols[C])
+				{
+					/*
+					sh_print_production(production);
+					ft_printf("\n");
+					sh_print_symbol(prod_symbol);
+					ft_printf(GREEN" follow sets:\n"EOC);
+					sh_process_print_set(cfg, prod_symbol->follow_sets);
+					ft_printf("\n");
 					sh_print_symbol(ref);
-		//		ft_printf("ouai\n");
+					ft_printf(CYAN" follow sets:\n"EOC);
+					sh_process_print_set(cfg, ref->follow_sets);
+					ft_printf("\n");
+					*/
+				ft_printf("\n");
+				ft_printf("ouai\n");
+				}
 				sh_process_fill_sets(prod_symbol->follow_sets,
 					ref->follow_sets, 0, &changes);
 			}
@@ -129,7 +164,6 @@ int		sh_process_follow_sets_symbol(t_cfg *cfg, t_symbol *symbol)
 {
 	int i;
 	int j;
-
 	int changes;
 
 	changes = 0;
@@ -137,7 +171,9 @@ int		sh_process_follow_sets_symbol(t_cfg *cfg, t_symbol *symbol)
 	j = 0;
 	while (j < NB_NOTERMS)
 	{
-		if (sh_process_follow_sets_symbol_ref(cfg, &cfg->symbols[i], symbol))
+		if (cfg->symbols[i].follow_sets[END_OF_INPUT]
+				&& sh_process_follow_sets_symbol_ref(cfg,
+					&cfg->symbols[i], symbol))
 			changes = 1;
 		i++;
 		j++;
