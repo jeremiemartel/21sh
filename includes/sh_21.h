81e9968528c2a6b69e04cee04b2ea00d84fb3434
Jeremie MARTEL <jmartel@e2r2p8.42.fr>
2019-04-13 11:25:22 +0200

    added two headers, sh_lexer.h sh_tokens.h to split the sh_21.h, need to add a new one for parser fonctionnalities

--- includes/sh_21.h
+++ includes/sh_21.h
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:59:26 by ldedier           #+#    #+#             */
-/*   Updated: 2019/04/13 11:10:10 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/04/13 11:23:28 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,217 +14,11 @@
 # define SH_21_H
 
 # include <stdarg.h>
-#include "libft.h"
-
 # include "libft.h"
+# include "sh_tokens.h"
+# include "sh_lexer.h"
 
 # define DEBUG_BUFFER	50
-# define NB_PRODUCTIONS	112
-# define NB_SYMBOLS 102
-
-typedef enum		e_tokenlist
-{
-	UNKNOWN = -1,
-	SPACE,
-	WHILE,
-	GREAT,
-	LESS,
-	WORD,
-}					t_tokenlist;
-
-
-typedef enum		e_symbol_id
-{
-	LEX_TOK_UNKNOWN = 0, //1
-	//simple operators
-	LEX_TOK_PIPE = '|', //2
-	LEX_TOK_AND = '&', //3
-	LEX_TOK_SEMICOL = ';', //4
-	LEX_TOK_LESS = '<', //5
-	LEX_TOK_GREAT = '>', //6
-	LEX_TOK_OPN_PAR = '(', //7
-	LEX_TOK_CLS_PAR = ')', //8 
-	LEX_TOK_LBRACE = '{', //9 
-	LEX_TOK_RBRACE = '}', //10
-	LEX_TOK_BANG = '!', //11
-	LEX_TOK_DOLLAR = '$', //12
-	//Quoting tokens
-	LEX_TOK_BACK_SLASH = '\\', //13
-	LEX_TOK_QUOTE_BACK = '`', //14
-	LEX_TOK_QUOTE_SPL = '\'', //15
-	LEX_TOK_QUOTE_DBL = '"', //16
-	LEX_TOK_SPACE = ' ', //17
-	LEX_TOK_TAB = '\t', //18
-	LEX_TOK_NEWLINE = '\n', //19
-	//Special chars
-	LEX_TOK_STAR = '*', //20
-	LEX_TOK_QUEST = '?', //21
-	LEX_TOK_HASH = '#', //22
-	LEX_TOK_TILD = '~', //23
-	LEX_TOK_EQUAL = '=', //24
-	LEX_TOK_PERCENT = '%', //25
-	//Composed operators
-	LEX_TOK_AND_IF = '&' + 0xff00 * '&', //26				//&&
-	LEX_TOK_OR_IF = '|' + 0xff00 * '|',	//27			//||
-	LEX_TOK_DSEMI = ';' + 0xff00 * ';',	//28			//;;
-	LEX_TOK_DLESS = '<' + 0xff00 * '<',	//29			//<<
-	LEX_TOK_DGREAT = '>' + 0xff00 * '>', //30				//>>
-	LEX_TOK_LESSAND = '<' + 0xff00 * '&', //31				//<&
-	LEX_TOK_GREATAND = '>' + 0xff00 * '&', //32				//>&
-	LEX_TOK_LESSGREAT = '<' + 0xff00 * '>',	//33			//<>
-	LEX_TOK_DLESSDASH = '<' + 0xff00 * '<' + 0xff0000 * '-',//34	//<<-
-	LEX_TOK_CLOBBER = '>' + 0xff00 * '|',	//35			//>| // index 34
-	
-	LEX_TOK_IF,
-	LEX_TOK_THEN,
-	LEX_TOK_ELSE,
-	LEX_TOK_ELIF,
-	LEX_TOK_FI,
-	LEX_TOK_DO,
-	LEX_TOK_DONE,
-	LEX_TOK_CASE,
-	LEX_TOK_ESAC,
-	LEX_TOK_WHILE,
-	LEX_TOK_UNTIL,
-	LEX_TOK_FOR,
-	LEX_TOK_IN, //index 47
-
-	//Other
-	LEX_TOK_TOKEN = 130,
-	LEX_TOK_WORD,
-	LEX_TOK_ASSIGNMENT_WORD, //index 50
-	LEX_TOK_NAME,
-	LEX_TOK_IO_NUMBER,
-
-		//Specials
-	END_OF_INPUT,
-	EPS, //index 54 (NB_TERMS = 55)
-
-	//Non Terminals
-
-	PROGRAM,
-	COMPLETE_COMMANDS,
-	COMPLETE_COMMAND,
-	LIST,
-	AND_OR,
-	PIPELINE,
-	PIPE_SEQUENCE,
-	COMMAND,
-	COMPOUND_COMMAND,
-	SUBSHELL,
-	COMPOUND_LIST,
-	TERM,
-	FOR_CLAUSE,
-	NAME,
-	IN,
-	WORDLIST,
-	CASE_CLAUSE,
-	CASE_LIST_NS,
-	CASE_LIST,
-	CASE_ITEM_NS,
-	CASE_ITEM,
-	PATTERN,
-	IF_CLAUSE,
-	ELSE_PART,
-	WHILE_CLAUSE,
-	UNTIL_CLAUSE,
-	FUNCTION_DEFINITION,
-	FUNCTION_BODY,
-	FNAME,
-	BRACE_GROUP,
-	DO_GROUP,
-	SIMPLE_COMMAND,
-	CMD_NAME,
-	CMD_WORD,
-	CMD_PREFIX,
-	CMD_SUFFIX,
-	REDIRECT_LIST,
-	IO_REDIRECT,
-	IO_FILE,
-	FILENAME,
-	IO_HERE,
-	HERE_END,
-	NEWLINE_LIST,
-	LINEBREAK,
-	SEPARATOR_OP,
-	SEPARATOR,
-	SEQUENTIAL_SEP, // + 47
-}					t_symbol_id;
-
-typedef struct		s_lexer
-{
-	char	c;
-	char	*input;
-	int		tok_start;
-	int		tok_len;
-	int		current_id;
-	int		quoted;
-	t_list	*list;
-}					t_lexer;
-
-/*
-** Expansion header
-*/
-typedef enum	e_exp_type
-{
-	LEX_EXP_ERR = -1,
-	LEX_EXP_OK = 0,
-	LEX_EXP_VAR,
-	LEX_EXP_PARAM,
-	LEX_EXP_CMD,
-	LEX_EXP_ARITH,
-}				t_exp_type;
-
-typedef struct	s_pattern
-{
-	char	start[4];
-	char	end[4];
-	int		len_s;
-	int		len_e;
-}				t_pattern;
-
-typedef struct s_expansion
-{
-	t_dystr		*res;
-	char		*original;
-	char		*expansion;
-	t_exp_type	type;
-	t_pattern	pattern;
-}				t_expansion;
-
-
-# define LEX_TOKEN_VALUE_LEN	250
-
-# define LEX_END		3
-# define LEX_ERR		2
-# define LEX_CONTINUE	1
-# define LEX_OK			0
-
-typedef struct		u_token_union
-{
-	int				ival;
-	double			fval;
-	char			*str;
-}					t_token_union;
-
-typedef enum		e_token_type
-{
-	TYPE_INT,
-	TYPE_FLOAT,
-	TYPE_STR
-}					t_token_type;
-
-typedef struct		s_token
-{
-	t_token_type	token_type;
-	t_token_union	token_union;
-	t_symbol_id		id;
-	int				index;
-	char			value[LEX_TOKEN_VALUE_LEN + 1];
-}					t_token;
-
-# define NB_TERMS	55
-# define NB_NOTERMS	NB_SYMBOLS - NB_TERMS
 
 typedef struct		s_symbol
 {
@@ -273,17 +67,6 @@ typedef struct s_cfg
 	t_symbol		symbols[NB_SYMBOLS];
 	t_production	productions[NB_PRODUCTIONS];
 }					t_cfg;
-
-/*
-** lexer.c
-*/
-
-int			lexer(char *input, t_list **tokens);
-
-void		ft_putstr_len(char *str, int len);
-
-
-int			sh_parser(t_list *tokens);
 
 typedef enum		e_action_enum
 {
@@ -338,63 +121,6 @@ typedef struct		s_grammar_holder
 
 t_grammar_holder	g_grammar[NB_SYMBOLS];
 
-
-/*
-** lexer_expansions.c
-*/
-int			lexer_expansion(t_lexer *lexer, char **input);
-
-/*
-** lexer_expansion_detect.c
-*/
-int			lexer_expansion_detect(char *input, t_expansion *expansion);
-int			lexer_expansion_detect_command(char *input, t_expansion *expansion);
-int			lexer_expansion_detect_arithmetic(char *input, t_expansion *expansion);
-int			lexer_expansion_detect_parameter(char *input, t_expansion *expansion);
-int			lexer_expansion_detect_variable(char *input, t_expansion *expansion);
-
-/*
-** lexer_expansion_process.c
-*/
-int			lexer_expansion_process(t_lexer *lexer, t_expansion *expansion);
-int			lexer_expansion_process_command(t_lexer *lexer, t_expansion *expansion);
-int			lexer_expansion_process_arithmetic(t_lexer *lexer, t_expansion *expansion);
-int			lexer_expansion_process_parameter(t_lexer *lexer, t_expansion *expansion);
-int			lexer_expansion_process_variable(t_lexer *lexer, t_expansion *expansion);
-
-
-/*
-** t_lexer.c
-*/
-void		lexer_init(t_lexer *lexer, int tok_start);
-int			lexer_add_token(t_lexer *lexer);
-void		lexer_show(t_lexer *lexer);
-void		t_lexer_free(t_lexer *lexer);
-
-/*
-** lexer_rules.c
-*/
-int			lexer_rule1(t_lexer *lexer);
-int			lexer_rule2(t_lexer *lexer);
-int			lexer_rule3(t_lexer *lexer);
-int			lexer_rule4(t_lexer *lexer);
-int			lexer_rule5(t_lexer *lexer);
-int			lexer_rule6(t_lexer *lexer);
-int			lexer_rule7(t_lexer *lexer);
-int			lexer_rule8(t_lexer *lexer);
-int			lexer_rule9(t_lexer *lexer);
-int			lexer_rule10(t_lexer *lexer);
-int			lexer_rule11(t_lexer *lexer);
-
-/*
-** t_token.c
-*/
-t_token		*t_token_new(int id, char *value);
-void		t_token_show(t_token *token);
-void		t_token_show_id(int i);
-int			sh_process_test(void);
-int			sh_parse_token_list(t_lr_parser *parser);
-int			sh_is_term(t_symbol *symbol);
 /*
 ** first_sets.c
 */
@@ -435,6 +161,9 @@ void	sh_print_ast_parser(t_lr_parser *parser);
 ** lr_parse.c
 */
 int		sh_lr_parse(t_lr_parser *parser);
+int			sh_process_test(void);
+int			sh_parse_token_list(t_lr_parser *parser);
+int			sh_is_term(t_symbol *symbol);
 
 /*
 ** compute_lr_automata.c
