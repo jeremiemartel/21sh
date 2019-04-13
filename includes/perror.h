894e5f480d66a24de427332f3cee117d88f2a5a1
Jeremie MARTEL <jmartel@e2r2p8.42.fr>
2019-04-13 15:13:48 +0200

    adding dy_tab conversion in main

--- includes/perror.h
+++ includes/perror.h
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 14:47:16 by jmartel           #+#    #+#             */
-/*   Updated: 2019/04/13 14:55:07 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/04/13 15:11:24 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 # define SH_NAME		"21sh"
 # define SH_ERR_COLOR	COLOR_PINK
 
-# define SH_ERR1_MALLOC	"Malloc error occured",
+# define SH_ERR1_MALLOC	"Malloc error occured"
+
+/*
+** ft_perror.c
+*/
+int		ft_perror(const char *error, const char *s1);
+void	*ft_perrorn(const char *error, const char *s1);
+int		ft_perror2(const char *error, const char *s1, const char *s2);
+void	*ft_perror2n(const char *error, const char *s1, const char *s2);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,50 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:59:53 by ldedier           #+#    #+#             */
-/*   Updated: 2019/03/18 06:02:31 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/04/13 15:13:26 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_21.h"
 
-int			main(int argc, char **argv)
+t_dy_tab	*main_init_env(char **env)
+{
+	int			len;
+	int			i;
+	t_dy_tab	*tbl;
+
+	len = ft_strtab_len(env);
+	if (!(tbl = ft_dy_tab_new(len + 1)))
+		return (ft_perrorn(SH_ERR1_MALLOC, "main_init_env"));
+	i = 0;
+	while (i < len)
+	{
+		if (ft_dy_tab_add_str(tbl, env[i]))
+		{
+			ft_dy_tab_del(tbl);
+			return (ft_perrorn(SH_ERR1_MALLOC, "main_init_env"));
+		}
+		i++;
+	}
+	return (tbl);
+}
+
+int			main(int argc, char **argv, char **original_env)
 {
 	char		*input;
 	t_list		*tokens;
+	t_dy_tab	*env;
 
 	tokens = NULL;
+	if (!(env = main_init_env(original_env)))
+		return (FAILURE);
 	if (argc == 1)
-		return (0);
+		return (SUCCESS);
 	input = ft_strdup(argv[1]);
 	if (lexer(input, &tokens))
-		return (1);
+		return (FAILURE);
 	if (sh_parser(tokens))
-		return (1);
+		return (FAILURE);
 	ft_strdel(&input);
-	return (0);
+	return (SUCCESS);
 }
