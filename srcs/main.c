241b766ae5c9662d66a40243c35dd5e5add0f9ae
Ludovic DEDIER <ldedier@e1r2p6.42.fr>
2019-04-15 13:54:24 +0200

    fixed main incorrect free

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 17:59:53 by jmartel           #+#    #+#             */
-/*   Updated: 2019/04/14 15:23:28 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/04/15 13:53:46 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int			main(int argc, char **argv, char **original_env)
 	}
 	if (sh_parser(tokens))
 		return (FAILURE);
-	t_lexer_free_token_list(tokens);
+//	t_lexer_free_token_list(tokens);
 	ft_dy_tab_del(env);
 	ft_strdel(&input);
 	return (SUCCESS);
