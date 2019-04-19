1ffa7b7ded2df6ad59c0f013c6cbda34bfafbf36
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-04-19 15:10:48 +0200

    dgreat added

--- srcs/traverse/sh_traverse_dgreat.c
+++ srcs/traverse/sh_traverse_dgreat.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 11:19:41 by jmartel           #+#    #+#             */
-/*   Updated: 2019/04/19 11:21:49 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/04/19 15:10:34 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 int		sh_traverse_dgreat(t_ast_node *node, t_context *context)
 {
+	char	*filename;
+	int		original_fd;
+	int		fd;
+
+	filename = ((t_ast_node*)((t_ast_node*)(node->children->next->content))->children->content)->token->value;
+	original_fd = context->father_id;
+	if (original_fd < 0)
+		original_fd = 1;
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (ft_perror("Can't open file", filename));
+	if (original_fd <= 2)
+		context->fd[original_fd] = fd;
+	else
+	{
+		if ((dup2(fd, original_fd)) == -1)
+			return (ft_perror(SH_ERR1_INTERN_ERR, "sh_traverse_great: dup2"));
+	}
 	return (SUCCESS);
-	(void)node;
-	(void)context;
 }
