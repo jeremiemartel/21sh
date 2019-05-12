20f89953d14f056cce3d5b3cfb29f9959fc4c2a5
Jeremie MARTEL <jmartel@e1r1p4.42.fr>
2019-05-12 15:03:06 +0200

    perror can now write on a specified file descriptor, to support new builtin models

--- includes/perror.h
+++ includes/perror.h
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/13 14:47:16 by jmartel           #+#    #+#             */
-/*   Updated: 2019/05/09 12:18:17 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/05/12 13:31:56 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,20 @@
 # define SH_ERR1_HISTORIC		"Could not open the historic file"
 # define SH_ERR1_PERM_DENIED	"Permission denied"
 # define SH_ERR1_TOO_MANY_ARGS	"Too many arguments"
+# define SH_ERR1_NOT_A_DIR		"Not a directory"
+
+# define SH_ERR2_NO_SUCH_FILE_OR_DIR	"No such file or directory"
 
 /*
 ** ft_perror.c
 */
-int		ft_perror(const char *error, const char *s1);
-void	*ft_perrorn(const char *error, const char *s1);
-int		ft_perror2(const char *error, const char *s1, const char *s2);
-void	*ft_perror2n(const char *error, const char *s1, const char *s2);
+int			ft_perror(const char *error, const char *s1);
+void		*ft_perrorn(const char *error, const char *s1);
+int			ft_perror_fd(int fd, const char *error, const char *s1);
+void		*ft_perrorn_fd(int fd, const char *error, const char *s1);
+int			ft_perror2(const char *error, const char *s1, const char *s2);
+void		*ft_perror2n(const char *error, const char *s1, const char *s2);
+int			ft_perror2_fd(int fd, const char *error, const char *s1, const char *s2);
+void		*ft_perror2n_fd(int fd, const char *error, const char *s1, const char *s2);
 
 #endif
