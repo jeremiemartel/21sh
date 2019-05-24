ae73e58019798204178590a3ba2903f09a49f042
Jeremie MARTEL <jmartel@e1r2p8.42.fr>
2019-05-24 15:59:11 +0200

    tests_scripts
    added cd tests

--- tests_scripts/test_builtins.sh
+++ tests_scripts/test_builtins.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/21 16:00:41 by jmartel           #+#    #+#              #
-#    Updated: 2019/05/23 17:58:08 by jmartel          ###   ########.fr        #
+#    Updated: 2019/05/24 15:57:56 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,11 +23,17 @@ launch "Echo (builtin)"
 	test 'echo $TERM $PATH'
 	test 'echo -n $TERM $PATH'
 
-launch "pwd ()"
+launch "pwd (builtin)"
 	test "pwd"
 	test "pwd | cat -e"
 	test "pwd | cat -e | wc"
 	test "PWD=" "OLDPWD=" "pwd"
+
+launch "cd (builtin)"
+	test "cd" "pwd"
+	test "cd ~/" "pwd" "cd .. ; pwd"
+	test "cd /" "cd ../.." "pwd"
+	test "cd ." "pwd" "cd ../../" "pwd"
 
 launch "set (builtin)"
 	test "set"
