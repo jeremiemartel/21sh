07e52222a8dd762915b141b0c3fef9bda08a726d
Ludovic DEDIER <ldedier@e3r8p12.42.fr>
2019-05-27 18:17:45 +0200

    fixed segv on uninitialised lists

--- srcs/traverse/sh_traverse_pipe_sequence.c
+++ srcs/traverse/sh_traverse_pipe_sequence.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 17:34:52 by ldedier           #+#    #+#             */
-/*   Updated: 2019/05/27 17:57:40 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/05/27 18:13:39 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,23 @@ static int sh_add_pipe_redirections(t_ast_node *from, t_ast_node *to)
 	if (sh_add_redirection(INPUT, 0, fds[PIPE_OUT],
 			&to->metadata.command_metadata.redirections))
 		return (FAILURE);
-	ft_dprintf(2, "new pipe : in : %d, out : %d\n", fds[PIPE_IN], fds[PIPE_OUT]);
+//	ft_dprintf(2, "new pipe : in : %d, out : %d\n", fds[PIPE_IN], fds[PIPE_OUT]);
 	return (SUCCESS);
+}
+
+static void sh_init_command_redirections_list(t_ast_node *node)
+{
+	t_list *ptr;
+	t_ast_node	*child;
+
+	ptr = (t_list *)node->children;
+	while (ptr != NULL)
+	{
+		child = ptr->content;
+		((t_ast_node *)(child->children->content))->metadata.command_metadata.redirections = NULL;
+		if ((ptr = ptr->next))
+			ptr = ptr->next;
+	}
 }
 
 static int	sh_process_pipe_redirections(t_ast_node *node)
@@ -55,6 +70,7 @@ static int sh_traverse_pipe_sequences_redirections(t_ast_node *node,
 	t_ast_node	*simple_command_node;;
 	t_list		*ptr;
 
+	sh_init_command_redirections_list(node);
 	if (sh_process_pipe_redirections(node))
 		return (FAILURE);
 	ptr = (t_list *)node->children;
