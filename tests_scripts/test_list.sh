1b022071433deee84964f1959cd27328d8b4f2bb
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-06-03 17:06:26 +0200

    added tests on lists : && || ;

---
+++ tests_scripts/test_list.sh
@@ -0,0 +1,34 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    test_list.sh                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/05/23 13:10:40 by jmartel           #+#    #+#              #
+#    Updated: 2019/06/03 11:25:50 by jmartel          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+launch "Lists of commands"
+
+## ; tests
+	test 'ls -la ; pwd'
+	test 'asd ; asd a ; qweqweqw ; ls -la'
+	test 'echo okalm ; ls nofile ; pwd ; asdqw ; cat nofile'
+
+## && tests
+	test 'ls && pwd'
+	test 'ls nofile && pwd && echo okalm'
+	test 'echo okalm && ls nofile && echo okalm'
+	test 'echo okalm && ls && echo okalm'
+	test 'ecasd okalm && ls && echo okalm'
+
+## || tests
+	test 'ls || pwd'
+	test 'ls nofile || echo okalm'
+	test 'ls nofile || echo okalm || echo okalm'
+	test 'ls -la || echo okalm || echo okalm'
+	test 'lsasdqw -la || echqweo okalm || echo olm'
+
+finish
