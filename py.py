c378633c3fdd228141f4023b8f93709e460d96b7
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-06-05 22:53:17 +0200

    py.py can now be used to create automatic headers, need to improve it to get it norme friendly

--- py.py
+++ py.py
@@ -1,19 +1,39 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 # cat includes/* | grep ');' > inc
 # cat srcs/**/** | grep -v '**' > s
 
-fd = open("inc")
-line = fd.readline()
-while (line != ""):
-	if (line[0] == '\t'):
-		line = fd.readline()
-		continue
-	tab = line.split("\t")
-	# print(tab)
-	line = tab[len(tab) - 1]
-	line = line.strip("\n;*")
-	tab = line.split("(")
-	line = tab[0]
-	print(line)
-	line = fd.readline()
+# fd = open("inc")
+# line = fd.readline()
+# while (line != ""):
+# 	if (line[0] == '\t'):
+# 		line = fd.readline()
+# 		continue
+# 	tab = line.split("\t")
+# 	# print(tab)
+# 	line = tab[len(tab) - 1]
+# 	line = line.strip("\n;*")
+# 	tab = line.split("(")
+# 	line = tab[0]
+# 	print(line)
+# 	line = fd.readline()
+
+import os
+import re;
+
+dir = "./srcs/expansions"
+header = "./includes/expansions.h"
+
+format = "^(void|int|char|t_*)"
+
+for filename in os.listdir(dir):
+	fd = open(os.path.join(dir, filename))
+	line1 = fd.readline()
+	print("/*")
+	print("**" + filename);
+	print("*/")
+	while (line1 != ""):
+		if (re.search(format, line1) != None):
+			print(line1.rstrip() + ';')
+		line1 = fd.readline()
+	print("")
