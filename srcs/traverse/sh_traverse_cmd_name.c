cc53d62672db76a187035f34e133de2e60a2c56c
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-06-07 14:38:16 +0200

    deleting to lower function in command names

--- srcs/traverse/sh_traverse_cmd_name.c
+++ srcs/traverse/sh_traverse_cmd_name.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 17:31:30 by ldedier           #+#    #+#             */
-/*   Updated: 2019/06/07 12:16:57 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/06/07 14:37:59 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@ int			sh_traverse_cmd_name(t_ast_node *node, t_context *context)
 	else if (context->phase == E_TRAVERSE_PHASE_EXECUTE)
 	{
 		child = (t_ast_node *)node->children->content;
-		if (ft_dy_tab_add_str(context->params,
-				ft_strtolower(child->token->value)))
+		if (ft_dy_tab_add_str(context->params, child->token->value))
 			return (FAILURE);
 		return (SUCCESS);
 	}
