729c06c52a3d5a1d06bc55f4cb9f3a401d830d72
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-06-07 16:35:55 +0200

    res.sh improved

--- tests_scripts/expansions/res.sh
+++ tests_scripts/expansions/res.sh
@@ -3,8 +3,17 @@
 old_log_file="./logs/prev"
 new_log_file="./logs/new"
 
-echo New Ok : `grep -c OK $new_log_file`
-echo Old Ok : `grep -c OK $old_log_file`
-echo ""
-echo New Ko : `grep -c KO $new_log_file`
-echo Old Ko : `grep -c KO $old_log_file`
+new_ok=`grep -c OK $new_log_file`
+old_ok=`grep -c OK $old_log_file`
+new_ko=`grep -c KO $new_log_file`
+old_ko=`grep -c KO $old_log_file`
+
+if [ "$old_ok" -ne "$new_ok" ] ; then
+	echo New Ok : $new_ok
+	echo Old Ok : $old_ok
+	echo ""
+	echo New Ko : $new_ko
+	echo Old Ko : $old_ko
+else
+	echo OK
+fi
