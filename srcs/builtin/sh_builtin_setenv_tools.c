55d025e8f7a47dc5f95b51b8d749a9893be6941b
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-06-17 11:44:06 +0200

    modified setenv builtin return values

--- srcs/builtin/sh_builtin_setenv_tools.c
+++ srcs/builtin/sh_builtin_setenv_tools.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   set_env.c                                          :+:      :+:    :+:   */
+/*   sh_builtin_setenv_tools.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/12 18:43:35 by ldedier           #+#    #+#             */
-/*   Updated: 2019/06/15 17:16:07 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/06/17 11:33:49 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_21.h"
 
-int		get_key_len(char *entry)
+static int		get_key_len(char *entry)
 {
 	int i;
 
@@ -26,29 +26,7 @@ int		get_key_len(char *entry)
 	return (-1);
 }
 
-int		sh_add_to_env(t_dy_tab *env, char *key, char *value)
-{
-	char	*entry;
-	size_t	i;
-
-	i = 0;
-	while (i < env->current_size)
-	{
-		if (is_key_of_entry(env->tbl[i], key))
-		{
-			ft_dy_tab_suppr_index(env, i);
-			break ;
-		}
-		i++;
-	}
-	if (!(entry = ft_strjoin_3(key, "=", value)))
-		return (sh_perror(SH_ERR1_MALLOC, "sh_add_to_env"));
-	if (ft_dy_tab_add_ptr(env, entry))
-		return (sh_perror(SH_ERR1_MALLOC, "sh_add_to_env"));
-	return (SUCCESS);
-}
-
-int		is_bad_assignment(char *entry, int *key_len)
+static int	is_bad_assignment(char *entry, int *key_len)
 {
 	int len;
 
@@ -56,14 +34,14 @@ int		is_bad_assignment(char *entry, int *key_len)
 		return (1);
 	if ((*key_len = get_key_len(entry)) <= 0)
 		return (1);
-	return (SUCCESS);
+	return (0);
 }
 
-int		sh_process_bad_assignment(t_context *context, char *entry)
+static int	sh_process_bad_assignment(t_context *context, char *entry)
 {
 	ft_dprintf(context->fd[FD_ERR], "%s: \'%s\' bad assignment\n",
 		SH_NAME, entry);
-	return (1);
+	return (ERROR);
 }
 
 int		sh_process_setenv_equal(char *entry, t_dy_tab *env, t_context *context)
@@ -75,17 +53,17 @@ int		sh_process_setenv_equal(char *entry, t_dy_tab *env, t_context *context)
 	if (is_bad_assignment(entry, &key_len))
 		return (sh_process_bad_assignment(context, entry));
 	if (!(key = ft_strndup(entry, key_len)))
-		return (-1);
+		return (sh_perror(SH_ERR1_MALLOC, "sh_process_setenv_equal (1)"));
 	if (!(value = ft_strnrest(entry, key_len + 1)))
 	{
 		free(key);
-		return (sh_perror(SH_ERR1_MALLOC, "sh_process_setenv_equal"));
+		return (sh_perror(SH_ERR1_MALLOC, "sh_process_setenv_equal (1)"));
 	}
-	if (sh_add_to_env(env, key, value))
+	if (sh_vars_assign_key_val(env, NULL, key, value) == FAILURE)
 	{
 		free(key);
 		free(value);
-		return (sh_perror(SH_ERR1_MALLOC, "sh_process_setenv_equal"));
+		return (sh_perror(SH_ERR1_MALLOC, "sh_process_setenv_equal (1)"));
 	}
 	if (!ft_strcmp(key, "PATH"))
 		process_builtin_hash_suppr_all(context->shell);
