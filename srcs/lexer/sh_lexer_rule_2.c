3aea45c34ee05e13de24065b07c1e48c4715f837
Jeremie MARTEL <jmartel@e1r2p17.42.fr>
2019-07-02 22:53:07 +0200

    fixed dlessdash detection in lexer

--- srcs/lexer/sh_lexer_rule_2.c
+++ srcs/lexer/sh_lexer_rule_2.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 14:25:01 by jmartel           #+#    #+#             */
-/*   Updated: 2019/06/10 14:56:35 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/07/02 22:42:00 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		sh_lexer_rule2(t_lexer *lexer)
 		return (LEX_CONTINUE);
 	if (!ft_strchr(operators, lexer->current_id & 0x00ff))
 		return (LEX_CONTINUE);
-	if (ft_strchr(operators, lexer->c))
+	if (ft_strchr(operators, lexer->c) || lexer->c == '-')
 	{
 		if (!(lexer->current_id & 0xff00))
 		{
@@ -32,7 +32,7 @@ int		sh_lexer_rule2(t_lexer *lexer)
 		else if (lexer->c == '-' && lexer->current_id == '<' + '<' * 0xff00)
 			lexer->current_id += 0xff0000 * lexer->c;
 		else
-			return (LEX_CONTINUE); // LEX_ERR ???
+			return (LEX_CONTINUE);
 		lexer->tok_len++;
 		return (LEX_OK);
 	}
