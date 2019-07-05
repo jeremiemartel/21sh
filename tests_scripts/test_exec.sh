54a016d6034f883d53a059d94817cc8f0acf11bd
Jeremie MARTEL <jmartel@e1r2p8.42.fr>
2019-07-05 16:50:48 +0200

    fixed some exec tests

--- tests_scripts/test_exec.sh
+++ tests_scripts/test_exec.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/21 16:00:41 by jmartel           #+#    #+#              #
-#    Updated: 2019/06/20 13:35:36 by jmartel          ###   ########.fr        #
+#    Updated: 2019/07/05 16:39:13 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,13 +21,13 @@ launch "exec"
 	test_launch './functions.sh'
 
 	launch "Permissions"
-	test_launch 'echo "echo lol" > file' 'chmod 777 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 600 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 500 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 400 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 300 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 200 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 100 file' './file' 'chmod 777 file ; rm file'
-	test_launch 'echo "echo lol" > file' 'chmod 000 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 777 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 600 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 500 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 400 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 300 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 200 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 100 file' './file' 'chmod 777 file ; rm file'
+	test_launch 'echo "#!/bin/bash" >file' 'echo "echo lol" >> file' 'chmod 000 file' './file' 'chmod 777 file ; rm file'
 
 finish
