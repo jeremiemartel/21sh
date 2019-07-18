4a499c08639ad74e7ec8f1fcf4d1aba8c059fd3c
Jérémie Martel <j.jeremie.martel@gmail.com>
2019-07-18 15:06:29 +0200

    fixed echo builtin : returned value, write calls are protected

--- srcs/builtin/sh_builtin_echo.c
+++ srcs/builtin/sh_builtin_echo.c
@@ -6,32 +6,49 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 11:57:59 by jmartel           #+#    #+#             */
-/*   Updated: 2019/07/08 12:38:45 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/07/18 11:36:04 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_21.h"
 
+static int	sh_builtin_echo_write(t_context * context, char *str)
+{
+	int		len;
+	int		ret;
+
+	len = ft_strlen(str);
+	ret = write(context->fd[FD_OUT], str, len);
+	if (ret == -1)
+	{
+		return (sh_perror2_err_fd(context->fd[FD_ERR], "echo", "write error", SH_ERR1_BAD_FD));
+	}
+	return (SUCCESS);
+}
+
 int		sh_builtin_echo(t_context *context)
 {
-	int i;
+	int 	i;
+	int		ret;
 
 	if (!context->params->tbl[1])
-	{
-		ft_putchar_fd('\n', context->fd[FD_OUT]);
-		return (SUCCESS);
-	}
+		return (sh_builtin_echo_write(context, "\n"));
 	i = 1;
 	while (context->params->tbl[i] && ft_strequ(context->params->tbl[i], "-n"))
 		i++;
-	while (context->params->tbl[i + 1])
+	ret = 0;
+	while (!ret && context->params->tbl[i + 1])
 	{
-		ft_putstr_fd(context->params->tbl[i], context->fd[FD_OUT]);
-		ft_putchar_fd(' ', context->fd[FD_OUT]);
+		ret = sh_builtin_echo_write(context, context->params->tbl[i]);
+		if (!ret)
+			ret = sh_builtin_echo_write(context, " ");
 		i++;
 	}
-	ft_putstr_fd(context->params->tbl[i], context->fd[FD_OUT]);
+	if (ret)
+		return (ERROR);
+	if (sh_builtin_echo_write(context, context->params->tbl[i]) == ERROR)
+		return (ERROR);
 	if (!ft_strequ(context->params->tbl[1], "-n"))
-		ft_putchar_fd('\n', context->fd[FD_OUT]);
+		return (sh_builtin_echo_write(context, "\n"));
 	return (SUCCESS);
 }
