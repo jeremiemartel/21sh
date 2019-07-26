23f4258b3727f492016648fea7552970ef41f5eb
Jeremie MARTEL <jmartel@e1r2p17.42.fr>
2019-07-27 00:51:26 +0200

    assigments improvec

--- srcs/traverse/sh_traverse_assigment_word.c
+++ srcs/traverse/sh_traverse_assigment_word.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/20 12:11:44 by jmartel           #+#    #+#             */
-/*   Updated: 2019/07/21 10:46:57 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/07/27 00:51:16 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,19 @@
 
 int		sh_traverse_assigment_word(t_ast_node *node, t_context *context)
 {
-	if (ft_strnstr(node->token->value, "PATH=", 5))
-		sh_builtin_hash_empty_table(context->shell);
-	return (sh_vars_assignment(context->env, context->vars,
-				node->token->value));
+	if (context->phase == E_TRAVERSE_PHASE_EXECUTE)
+	{
+		if (node && node->token && node->token->expansion)
+			return (sh_expansions(context, node));
+		return (SUCCESS);
+	}
+	else if (context->phase == E_TRAVERSE_PHASE_EXECUTE)
+	{
+		if (ft_strnstr(node->token->value, "PATH=", 5))
+			sh_builtin_hash_empty_table(context->shell);
+		return (sh_vars_assignment(context->env, context->vars,
+					node->token->value));
+	}
+	else
+		return (sh_traverse_tools_browse(node, context));
 }
