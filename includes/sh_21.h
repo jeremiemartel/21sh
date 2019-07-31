fde661e377983d497c588a260311c46940c286d6
Jeremie MARTEL <jmartel@e1r2p16.42.fr>
2019-07-31 17:10:47 +0200

    unactivated tilde expansion bonus

--- includes/sh_21.h
+++ includes/sh_21.h
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/30 15:48:56 by jmartel           #+#    #+#             */
-/*   Updated: 2019/07/31 10:40:51 by ldedier          ###   ########.fr       */
+/*   Updated: 2019/07/31 16:20:17 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,7 +131,7 @@
 # define BONUS_HOME_AS_TILDE_PROMPT	1
 # define BONUS_REDIRECT_SIGNAL		1
 # define BONUS_DOLLAR_VARIABLE		1
-# define BONUS_TILDE_EXP			1
+# define BONUS_TILDE_EXP			0
 
 typedef struct s_shell		t_shell;
 
