088f523ec04656b2ce815826c56509ee27fc9ac2
Jeremie MARTEL <jmartel@e1r4p22.42.fr>
2019-08-06 19:39:41 +0200

    added execve fail error message

--- srcs/builtin/sh_builtin_env_process.c
+++ srcs/builtin/sh_builtin_env_process.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/29 16:31:43 by jmartel           #+#    #+#             */
-/*   Updated: 2019/07/23 03:09:30 by jmartel          ###   ########.fr       */
+/*   Updated: 2019/08/06 19:36:31 by jmartel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,8 +67,12 @@ static int	sh_builtin_env_process_no_slash
 	{
 		context->env = new_env;
 		if (context->current_pipe_sequence_node)
+		{
 			execve(context->path,
 			(char**)context->params->tbl, (char**)context->env->tbl);
+			sh_perror(((char**)context->params->tbl)[0], SH_ERR1_EXECVE_FAIL);
+			return (SH_RET_NOT_EXECUTABLE);
+		}
 		else
 			return (sh_process_execute(context));
 	}
@@ -108,7 +112,8 @@ static int	sh_builtin_env_process_slash(t_context *context, t_dy_tab *new_env)
 		{
 			execve(context->path,
 			(char**)context->params->tbl, (char**)context->env->tbl);
-			ret = SH_RET_CMD_NOT_FOUND;
+			sh_perror(((char**)context->params->tbl)[0], SH_ERR1_EXECVE_FAIL);
+			ret = SH_RET_NOT_EXECUTABLE;
 		}
 		else
 			ret = sh_process_execute(context);
