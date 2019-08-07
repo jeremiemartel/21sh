1b1a42b4c4bbf4828f5697738ea42c0fe5ddbef3
Jeremie MARTEL <jmartel@e1r4p14.42.fr>
2019-08-07 16:01:01 +0200

    added tests with segfault executable

--- tests_scripts/test_random.sh
+++ tests_scripts/test_random.sh
@@ -6,7 +6,7 @@
 #    By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/05/21 16:00:41 by jmartel           #+#    #+#              #
-#    Updated: 2019/07/07 14:45:29 by jmartel          ###   ########.fr        #
+#    Updated: 2019/08/07 15:57:50 by jmartel          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,4 +18,18 @@ launch "Shell"
 	test_launch '././..' '../../.'
 	test_launch 'echo tamer > file' 'echo okalm >> file' '<file cat' '<file cat >file' 'cat file' 'rm file'
 	test_launch 'env -i TERM=$TERM PATH=/bin ./21sh' 'cd includes ; pwd ; cd - ; pwd'
+
+	launch "segfault"
+	if [ ! -e ".segfault" ] ; then
+		echo -e ${red}"Can't find .segfault executable"${eoc}
+	else
+		test_launch './.segfault'
+		test_launch './.segfault | wc'
+		test_launch 'ls | ./.segfault'
+		test_launch './.segfault && echo $?'
+		test_launch './.segfault || echo $?'
+		test_launch './.segfault ; echo $?'
+		test_launch './.segfault | wc && echo $?'
+		test_launch 'ls | ./.segfault && echo $?'
+	fi
 finish
